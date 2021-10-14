b787b7e73dccb15cfcbb416415fa1dabbb39f9dc
SrCost <cst.flavio@gmail.com>
2021-10-13 23:58:00 -0300

    componente inicio sem consumo

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    if(environment.token == ''){
+      alert('Sua sessão expirou, realizar o login novamente.')
+      this.router.navigate(['/entrar'])
+    }
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { UserLogin } from '../model/UserLogin';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
@@ -19,4 +20,13 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
+
+  logado(){
+    let ok: boolean = false
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
+  }
+
 }
